da6909f4ef05db34b0c2dd7129d85f66a4bdfba6
Seshadri <seshadri1997@gmail.com>
2020-07-22 01:56:15 +0530

    star-a: First run.

--- star-a/FileBase.cpp
+++ star-a/FileBase.cpp
@@ -1,9 +1,23 @@
-#include<vector>
-#include<string>
-#include<fstream>
-
-#incude "FileBase.h"
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "FileBase.h"
 
 class FileBase{
-	vector<node> bubble;
+	std::vector<node> bubble;
+
+	void open_file (const std::string& file_name);
+	void close_file (const std::string& file_name);
 };
+
+void FileBase::open_file (const std::string& file_name){
+}
+
+void FileBase::close_file (const std::string& file_name){
+}
+
+int main(){
+	std::cout<<"A run without error!";
+	return 0;
+}
--- star-a/FileBase.h
+++ star-a/FileBase.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include<string>
+#include <string>
 
 #define NAME = "Name"
 #define ID   = "Id"
